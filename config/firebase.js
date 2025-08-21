720a671807095d839226093b976fd8fccf290ca6
Brian Kimathi <kimathibrian71@gmail.com>
2025-08-21 14:10:25 +0300

    shared auth cross authentication

--- config/firebase.js
+++ config/firebase.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./firebase-adminsdk.json");
+const serviceAccount = require("../firebase-adminsdk.json");
 
 try {
   const app = admin.initializeApp({
